e77f293d28f11dd1a25c2dca7f86c194fc28f79f
phantomcorn <jj.phantakorn@outlook.com>
2025-07-22 22:29:40 +0700

    pass correct payload to api

--- src/component/Modal/Modal.jsx
+++ src/component/Modal/Modal.jsx
@@ -7,7 +7,7 @@ export default function Modal({user, closeModal}) {
     const [update, status] = useUpdateUserMutation()
     const handleSubmit = async (values) => {
         try {
-            const res = await update(values).unwrap()
+            await update({id: user._id, ...values}).unwrap()
         } catch (err) {
             console.log(err)
         }
@@ -31,28 +31,35 @@ export default function Modal({user, closeModal}) {
                 onSubmit={async (values) => handleSubmit(values)}
             >
                 <Form className="modal shadow" onClick={(e) => e.stopPropagation()}>
-                    <div className='input-group'>
-                        <label htmlFor='email'>Email</label>
-                        <Field className="text-field" id="email" name="email" type="email"/>
+
+                    <div className="row edit-form-row">
+                        <div className='input-group'>
+                            <label htmlFor='email'>Email</label>
+                            <Field className="text-field" id="email" name="email" type="email"/>
+                        </div>
+
+                        <div className='input-group'>
+                            <label htmlFor='name'>Name</label>
+                            <Field className="text-field" id="name" name="name" type="name"/>
+                        </div>
+                    </div>
+                    
+                    <div className="row edit-form-row">
+                        <div className='input-group'>
+                            <label htmlFor='role'>Role</label>
+                            <Field className="text-field" id="role" name="role" type="role"/>
+                        </div>
+
+                        <div className='input-group'>
+                            <label htmlFor='birthdate'>Date of birth</label>
+                            <Field className="birthdate" id="birthdate" name="birthdate" type="birthdate" />
+                        </div>
                     </div>
 
-                    <div className='input-group'>
-                        <label htmlFor='name'>Name</label>
-                        <Field className="text-field" id="name" name="name" type="name"/>
+                    <div className="button-options">
+                        <button type="submit" className="secondary-btn">Save</button>
+                        <button onClick={handleClose}>Cancel</button>
                     </div>
-
-                    <div className='input-group'>
-                        <label htmlFor='role'>Role</label>
-                        <Field className="text-field" id="role" name="role" type="role"/>
-                    </div>
-
-                    <div className='input-group'>
-                        <label htmlFor='birthdate'>Date of birth</label>
-                        <Field className="birthdate" id="birthdate" name="birthdate" type="birthdate" />
-                    </div>
-
-                    <button type="submit" className="secondary-btn">Save</button>
-                    <button onClick={handleClose}>Cancel</button>
                 </Form>
             </Formik>
         </div>
