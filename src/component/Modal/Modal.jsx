9013760349d92d30a28e85a836982f57ba70ef59
phantomcorn <jj.phantakorn@outlook.com>
2025-07-24 19:00:13 +0700

    update password disabled logic

--- src/component/Modal/Modal.jsx
+++ src/component/Modal/Modal.jsx
@@ -102,7 +102,7 @@ export default function Modal({user, closeModal}) {
                             
                             <div className='flex flex-col'>
                                 <label htmlFor='password'>Password</label>
-                                <Field disabled={user ? true : false} className="text-field" id="password" name="password" type="password"/>
+                                <Field disabled={(isEdit && !user) ? false : true } className="text-field" id="password" name="password" type="password"/>
                                 {errors.password && touched.password ? (
                                     <div className="text-red-500">{errors.password}</div>
                                 ) : null}
